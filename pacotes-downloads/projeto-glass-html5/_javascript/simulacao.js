187821fb9d60756a91f1f8d87c4f7d0d09f9c369
LuciaRodrigues1 <LuciaRodrigues1@users.noreply.github.com>
2020-07-07 21:59:38 -0300

    Create simulacao.js
    Testando simulações

---
+++ pacotes-downloads/projeto-glass-html5/_javascript/simulacao.js
@@ -0,0 +1,216 @@
+var simulacoes = {};
+
+simulacoes["fisica"] = {
+  "trabalho_energia": {
+    "titulo": "Trabalho e Energia",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "energia_movimento": {
+    "titulo": "Energia e Movimento",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "som_ondas": {
+    "titulo": "Som e Ondas",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "calor_termodinamica": {
+    "titulo": "Calor e Termodinâmica",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "luz_radiacao": {
+    "titulo": "Luz e Radiação",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "eletricidade_magnetismo": {
+    "titulo": "Eletricidade e Magnetismo",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  }
+};
+
+simulacoes["fisica"]["luz_radiacao"].simulacoes.push({
+  "id": "espectro_de_corpo_negro",
+  "titulo": "Espectro de Corpo Negro",
+  "icone": "home",
+  "subtitulo": "Corpo Negro, Lei de Planck, Lei de Wien, Radiação Eletromagnética, Mecânica Quântica, Astronomia",
+  "descricao_curta": "Como o espectro do corpo negro do sol se compara à luz visível? Aprenda sobre o espectro de corpo negro da Sirius A, do Sol, de uma lâmpada e da Terra. Ajuste a temperatura para ver o comprimento de onda e a intensidade da mudança do espectro. Veja a cor do pico da curva espectral. ",
+  "descricao_completa": "Na Física, um corpo negro é um objeto hipotético que absorve toda a radiação eletromagnética que nele incide: nenhuma luz o atravessa e nem é refletida. Um corpo com essa propriedade, em princípio, não poderia ser visto, daí o nome corpo negro. Apesar do nome, corpos negros emitem radiação, o que permite determinar sua temperatura. Em equilíbrio termodinâmico, um corpo negro ideal irradia energia na mesma taxa que a absorve, sendo essa uma das propriedades que o tornam uma fonte ideal de radiação térmica. Na natureza não existem corpos negros perfeitos, já que nenhum objeto consegue ter absorção e emissão perfeitas.",
+  "link": "https://pt.wikipedia.org/wiki/Corpo_negro",
+  "iframe_link": "https://phet.colorado.edu/sims/html/blackbody-spectrum/latest/blackbody-spectrum_pt_BR.html"
+});
+
+simulacoes["fisica"]["som_ondas"].simulacoes.push({
+  "id": "ondas",
+  "titulo": "Ondas",
+  "icone": "waves",
+  "subtitulo": "Frequência, Amplitude, Velocidade de Onda, Comprimento de Onda, Água, Som, Luz",
+  "descricao_curta": "Faça ondas com uma torneira pingando, alto-falante ou laser! Ajuste a frequência e a amplitude e observe os efeitos. Ouça o som produzido pelo alto-falante, e descubra o que determina a cor da luz.",
+  "descricao_completa": "Em física, uma onda é uma perturbação oscilante de alguma grandeza física no espaço e periódica no tempo. A oscilação espacial se caracteriza por seu comprimento de onda, enquanto que o tempo decorrido em uma oscilação completa é denominado período da onda, e é o inverso da sua frequência. O comprimento de onda e a frequência estão relacionadas pela velocidade com que a onda se propaga. Fisicamente, uma onda é um pulso energético que se propaga através do espaço ou através de um meio (líquido, sólido ou gasoso), com velocidade definida. Segundo alguns estudiosos e até agora observado, nada impede que uma onda magnética se propague no vácuo ou através da matéria, como é o caso das ondas eletromagnéticas no vácuo ou dos neutrinos através da matéria, onde as partículas do meio oscilam à volta de um ponto médio mas não se deslocam. Exceto pela radiação eletromagnética, e provavelmente as ondas gravitacionais, que podem se propagar através do vácuo, as ondas existem em um meio cuja deformação é capaz de produzir forças de restauração através das quais elas viajam e podem transferir energia de um lugar para outro sem que qualquer das partículas do meio seja deslocada; isto é, a onda não transporta matéria. Há, entretanto, oscilações sempre associadas ao meio de propagação",
+  "link": "https://pt.wikipedia.org/wiki/Onda",
+  "iframe_link": "https://phet.colorado.edu/sims/html/waves-intro/latest/waves-intro_pt_BR.html"
+});
+
+simulacoes["fisica"]["eletricidade_magnetismo"].simulacoes.push({
+  "id": "cargas_e_campos",
+  "titulo": "Cargas e Campos",
+  "icone": "home",
+  "subtitulo": "Campo Elétrico, Eletrostática, Equipotencial, Potencial Eletrostático, Cargas Elétricas, Voltagem",
+  "descricao_curta": "Organize cargas positivas e negativas no espaço e veja o campo elétrico e potencial eletrostático resultantes. Trace linhas equipotenciais e descubra sua relação com o campo elétrico. Crie modelos de dipolos, capacitores e muito mais!",
+  "descricao_completa": "Um campo elétrico é o campo de força provocado pela ação de cargas elétricas, (elétrons, prótons ou íons) ou por sistemas delas. Cargas elétricas colocadas num campo elétrico estão sujeitas à ação de forças elétricas, de atração e repulsão. O campo elétrico sempre nasce nas cargas positivas (vetor) e morre nas cargas negativas. Isso explica o sentido do vetor mencionado acima. Quando duas cargas positivas são colocadas próximas uma da outra, o campo elétrico é de afastamento, gerando uma região entre as duas cargas isenta de campo elétrico. O mesmo ocorre para cargas negativas, com a diferença de o campo elétrico ser de aproximação. Já quando são colocadas próximas uma carga positiva e uma negativa, o campo nasce na primeira, e morre na segunda.",
+  "link": "https://pt.wikipedia.org/wiki/Campo_el%C3%A9trico",
+  "iframe_link": "https://phet.colorado.edu/sims/html/charges-and-fields/latest/charges-and-fields_pt_BR.html"
+});
+
+simulacoes["fisica"]["calor_termodinamica"].simulacoes.push({
+  "id": "formas_de_energia_e_tranfomacoes",
+  "titulo": "Formas de Energia e Transformações",
+  "icone": "home",
+  "subtitulo": "Energia de Conservação, Sistemas de Energia, Transferência de Energia",
+  "descricao_curta": "Explore como aquecer e resfriar o ferro, tijolo, água e azeite adiciona ou remove energia. Veja como a energia é transferida entre os objetos. Construa o seu próprio sistema, com fontes de energia, trocadores e usuários. Acompanhe e visualize como a energia flui e muda através de seu sistema.",
+  "descricao_completa": "A transformação de energia é definida como o processo de mudança de energia de uma forma para outra. Existem tantos tipos diferentes de energia que podem se transformar de uma forma a outra. Existe energia a partir de reações químicas chamadas energia química, energia de processos térmicos chamados energia de calor e energia de partículas carregadas chamadas energia elétrica. Os processos de fissão, que são átomos de divisão e fusão, que combinam átomos, nos dão outro tipo de energia chamada energia nuclear. E, finalmente, a energia do movimento, a energia cinética e a energia associada à posição, energia potencial, são coletivamente chamadas de energia mecânica.",
+  "link": "https://www.portalsaofrancisco.com.br/fisica/transformacao-de-energia",
+  "iframe_link": "https://phet.colorado.edu/sims/html/energy-forms-and-changes/latest/energy-forms-and-changes_pt.html"
+});
+
+simulacoes["fisica"]["energia_movimento"].simulacoes.push({
+  "id": "adicao_de_vetores",
+  "titulo": "Adição de Vetores",
+  "icone": "home",
+  "subtitulo": "Vetores, Componentes de Vetores, Soma de Vetores, Equações",
+  "descricao_curta": "Explore vetores em 1D ou 2D e descubra como os vetores se somam. Especifique vetores em coordenadas cartesianas ou polares e veja a magnitude, o ângulo e os componentes de cada vetor. Experimente equações vetoriais e compare somas e diferenças vetoriais.",
+  "descricao_completa": "Vetores são setas que têm como características a direção, o módulo e o sentido. Na Física, além dessas características, os vetores têm nomes. Isso porque eles representam grandezas (força, aceleração, por exemplo). A adição de vetores pode ser feita através de duas regras, seguindo os seguintes passos: Regra do Paralelogramo 1.º Junte as origens dos vetores. 2.º Trace uma linha paralela a cada um dos vetores, formando um paralelogramo. 3.º Some a diagonal do paralelogramo.",
+  "link": "https://www.todamateria.com.br/vetores-fisica-matematica/",
+  "iframe_link": "https://phet.colorado.edu/sims/html/vector-addition/latest/vector-addition_pt_BR.html"
+});
+
+simulacoes["fisica"]["trabalho_energia"].simulacoes.push({
+  "id": "propriedades_dos_gases",
+  "titulo": "Propriedades dos Gases",
+  "icone": "home",
+  "subtitulo": "Lei do Gás Ideal, Teoria Molecular Cinética, Difusão",
+  "descricao_curta": "Bombeie moléculas de gás em uma caixa e veja o que acontece quando você altera o volume, adiciona ou remove o calor, muda a gravidade, e muito mais. Meça a temperatura e a pressão, e descubra como as propriedades do gás variam entre si. Examine os histogramas de energia cinética e de velocidade das partículas pesadas e da luz. Explore a difusão e determine como a concentração, temperatura, massa e raio afetam a taxa de difusão.",
+  "descricao_completa": "As leis de gás foram criadas no final do século XVIII, quando os cientistas começaram a perceber que nas relações entre a pressão, o volume e a temperatura de uma amostra de gás pode ser obtida uma fórmula que seria válida para todos os gases. Eles comportam-se de forma semelhante numa ampla variedade de condições, devido à boa aproximação com moléculas que estão mais afastadas, e agora a equação de estado para um gás ideal é derivada da teoria cinética. Agora as leis anteriores de gás são como casos especiais da equação do gás ideal, com uma ou mais das variáveis mantidas constantes.",
+  "link": "https://pt.wikipedia.org/wiki/Vetor_(matem%C3%A1tica)",
+  "iframe_link": "https://phet.colorado.edu/sims/html/gas-properties/latest/gas-properties_pt_BR.html"
+});
+
+simulacoes["fisica"]["eletricidade_magnetismo"].simulacoes.push({
+  "id": "lei_de_coulomb",
+  "titulo": "Lei de Coulomb",
+  "icone": "bug_report",
+  "subtitulo": "lei da física que descreve a interação eletrostática entre partículas eletricamente carregadas.",
+  "descricao_curta": "A Lei de Coulomb é uma lei da física que descreve a interação eletrostática entre partículas eletricamente carregadas. Foi formulada e publicada pela primeira vez em 1783 pelo físico francês Charles Augustin de Coulomb e foi essencial para o desenvolvimento do estudo da Eletricidade.[1]",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Lei_de_Coulomb",
+  "iframe_link": "https://phet.colorado.edu/sims/html/coulombs-law/latest/coulombs-law_pt_BR.html"
+});
+
+simulacoes["fisica"]["eletricidade_magnetismo"].simulacoes.push({
+  "id": "lei_de_faraday",
+  "titulo": "Lei de Faraday",
+  "icone": "home",
+  "subtitulo": "Lei da fisica que descreve a interação do campo elétrico com o magnético",
+  "descricao_curta": "Lei da indução de Faraday, ou simplesmente, lei da indução eletromagnética, é uma das equações básicas do eletromagnetismo. Ela prevê como um campo magnético interage com um circuito elétrico para produzir uma força eletromotriz — um fenômeno chamado de indução eletromagnética. ",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Lei_de_Faraday-Neumann-Lenz",
+  "iframe_link": "https://phet.colorado.edu/sims/html/faradays-law/latest/faradays-law_pt.html"
+});
+
+simulacoes["fisica"]["eletricidade_magnetismo"].simulacoes.push({
+  "id": "lei_de_ohm",
+  "titulo": "Lei de Ohm",
+  "icone": "home",
+  "subtitulo": "Lei da fisica que descreve a razão entre a tensão entre dois pontos e a corrente elétrica é constante",
+  "descricao_curta": "Lei que afirma que, para um condutor mantido à temperatura constante, a razão entre a tensão entre dois pontos e a corrente elétrica é constante. Essa constante é denominada de resistência elétrica.",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Lei_de_Ohm",
+  "iframe_link": "https://phet.colorado.edu/sims/html/ohms-law/latest/ohms-law_pt.html"
+});
+simulacoes["fisica"]["luz_radiacao"].simulacoes.push({
+  "id": "lei_de_coulomb",
+  "titulo": "Lei de Snell",
+  "icone": "bug_report",
+  "subtitulo": "Em ótica, a lei de Snell, ou simplesmente lei de refração, resume-se a uma expressão que dá o desvio angular sofrido por um raio de luz ao passar para um meio com índice de refração diferente do qual ele estava percorrendo.",
+  "descricao_curta": "Em ótica, a lei de Snell, ou simplesmente lei de refração, resume-se a uma expressão que dá o desvio angular sofrido por um raio de luz ao passar para um meio com índice de refração diferente do qual ele estava percorrendo. Em outras palavras, descreve a relação entre os ângulos de incidência e refração, quando referindo-se a luz ou outras ondas passando através de uma fronteira (interface) entre dois meios isotrópicos diferentes, tais como água e vidro. A lei de Snell-Descartes refere-se aos cientistas Willebrord Snellius e René Descartes.",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Lei_de_Snell",
+  "iframe_link": "https://phet.colorado.edu/sims/html/bending-light/latest/bending-light_pt_BR.html"
+});
+
+simulacoes["fisica"]["luz_radiacao"].simulacoes.push({
+  "id": "moleculas_e_luz",
+  "titulo": "Moléculas e luz",
+  "icone": "home",
+  "subtitulo": "Moléculas, Fótons, Absorção, Luz.",
+  "descricao_curta": "Explorar como a luz interage com as moléculas na atmosfera, Identificar que a absorção da luz depende da molécula e do tipo de luz, Relacionar a energia da luz ao movimento resultante, Identificar que energia aumenta do micro-ondas ao ultravioleta, Prever o movimento de uma molécula com base no tipo de luz que ela absorve, Identificar como a estrutura de uma molécula afeta a forma como ela interage com a luz.",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Radia%C3%A7%C3%A3o_eletromagn%C3%A9tica",
+  "iframe_link": "https://phet.colorado.edu/sims/html/molecules-and-light/latest/molecules-and-light_pt.html"
+});
+
+simulacoes["fisica"]["calor_termodinamica"].simulacoes.push({
+  "id": "atrito",
+  "titulo": "Atrito",
+  "icone": "home",
+  "subtitulo": "Atrito, Termodinâmica, Calor",
+  "descricao_curta": "Atrito, em física, é a força de contato que atua sempre que dois corpos entram em choque e há tendência ao movimento. É gerada pela aspericidade (rugosidade) dos corpos.",
+  "descricao_completa": "Atrito, em física, é a força de contato que atua sempre que dois corpos entram em choque e há tendência ao movimento. É gerada pela aspericidade (rugosidade) dos corpos . A força de atrito é sempre paralela às superfícies em interação e geralmente contrária ao movimento relativo entre elas. Apesar de sempre paralelo às superfícies em interação, o atrito entre estas superfícies depende da força normal, a componente vertical da força de contato; quanto maior for a força normal maior será o atrito.",
+  "link": "https://pt.wikipedia.org/wiki/Atrito",
+  "iframe_link": "https://phet.colorado.edu/sims/html/friction/latest/friction_pt_BR.html"
+});
+
+simulacoes["fisica"]["eletricidade_magnetismo"].simulacoes.push({
+  "id": "baloes_e_eletricidade_estatica",
+  "titulo": "Balões e Eletricidade Estática",
+  "icone": "home",
+  "subtitulo": "Eletricidade Estática, Cargas Elétricas, Força Elétrica",
+  "descricao_curta": "Utilize um balão para explorar conceitos de eletricidade estática, como transferência de carga, atração, repulsão e carga induzida.",
+  "descricao_completa": "Esta simulação demonstra como ocorre a eletrização de um balão ao ser atritado a uma blusa de lã. Demonstra ainda a atração entre objetos eletrizados com cargas contrárias e a repulsão entre objetos de mesma carga",
+  "link": "http://acervodigital.unesp.br/handle/unesp/378001",
+  "iframe_link": "https://phet.colorado.edu/sims/html/balloons-and-static-electricity/latest/balloons-and-static-electricity_pt_BR.html"
+});
+
+simulacoes["fisica"]["som_ondas"].simulacoes.push({
+  "id": "ondas_em_corda",
+  "titulo": "Ondas em Corda",
+  "icone": "waves",
+  "subtitulo": "Ondas, Frequência, Amplitude, Damping",
+  "descricao_curta": "Explore o maravilhoso mundo das ondas! Observe também uma corda vibrar em câmera lenta. Mexa a ponta de uma corda e faça ondas, ou ajuste a frequência e a amplitude de um oscilador.",
+  "descricao_completa": "Quando uma corda é deformada, a perturbação propaga-se por toda a corda, refletindo-se nas suas extremidades fixas. A interferência de duas ondas senoidais iguais que se propagam em sentidos opostos produz uma onda estacionária, ou seja, uma oscilação que aparenta não se mover através do material. Os nodos resultam da interferência (destrutiva) entre a crista e o vale de duas ondas. Nos anti-nodos, onde o deslocamento é máximo, a interferência dá-se entre duas cristas ou dois vales de onda. Cada padrão de oscilação corresponde a uma determinada frequência a que se chama um harmônico. As frequências de vibração variam com o comprimento da corda e com as suas características (material, tensão, espessura), que determinam a velocidade de propagação das ondas. À frequência mais baixa a que a corda vibra chama-se frequência fundamental.",
+  "link": "https://pt.wikipedia.org/wiki/OndasEstacionaria",
+  "iframe_link": "https://phet.colorado.edu/sims/html/wave-on-a-string/latest/wave-on-a-string_pt.html"
+});
+
+simulacoes["fisica"]["trabalho_energia"].simulacoes.push({
+  "id": "massas_e_molas",
+  "titulo": "Massas e Molas",
+  "icone": "home",
+  "subtitulo": "Movimento Periódico, Lei de Hooke, Energia de Conservação, Leis de Newton, Medida, Vetores",
+  "descricao_curta": "Pendure massas nas molas e ajuste a constante e o amortecimento da mola. Transporte o laboratório para diferentes planetas ou diminua o tempo. Observe as forças e a energia no sistema em tempo real e use o cronômetro para medir o período.",
+  "descricao_completa": "O sistema massa-mola é constituído por um corpo de massa m acoplado a uma mola com fator restaurador k (constante de deformação), enquanto a outra extremidade está ligada a um ponto fixo conforme mostrado na Figura 1. Se tal sistema encontra-se em equilíbrio, a posição da massa é denotada por O (x = 0) e toda vez que a massa é deslocada em relação a ponto, surge uma força restauradora F = - kx, que tenta trazê-la de volta à situação inicial. As posições -xM e xM representam, respectivamente, a mola comprimida e a mola estendida. Quando o bloco de massa m é deslocado em relação à posição inicial e solto em seguida, o sistema passa a oscilar em torno da posição de equilíbrio.",
+  "link": "https://pt.wikipedia.org/wiki/Sistema_massa-mola",
+  "iframe_link": "https://phet.colorado.edu/sims/html/masses-and-springs/latest/masses-and-springs_pt_BR.html"
+});
